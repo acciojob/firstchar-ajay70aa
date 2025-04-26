ed1d0b0ca219d12025799e81b7ba86fc3e4a752a
D. Ajay Kumar <154305661+ajay70aa@users.noreply.github.com>
2025-04-26 18:45:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ function firstChar(text) {
   // your code here
 	if (typeof(text) !== 'string') return ''
 	text = text.trim()[0]
-	return text.length() > 0 ? text[0] : ''
+	return text.length > 0 ? text[0] : ''
 }
 
 // Do not change the code below
 //Uncomment the following line to show the prompt popup
-const text = promp("Enter text:");
+const text = prompt("Enter text:"); 
 alert(firstChar(text));
